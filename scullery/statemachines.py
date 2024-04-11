8d9374056d52bd3c2be31e31cc22b6309ae4e0e5
Daniel Dunn <dannydunn@example.com>
2024-04-11 17:14:50 -0600

    snake_compat and py.typed

--- scullery/statemachines.py
+++ scullery/statemachines.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import time
 import threading
 import weakref
+
 from typeguard import typechecked
 from typing import Callable, Any
 from scullery import workers, util, scheduling
